4cd541fe4d8e07a285ea69be4ea9d5ee20a3372b
boer <boer0924@qq.com>
2016-07-26 23:47:10 +0800

    2th night work

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,6 +1,11 @@
 from . import auth
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
+from flask_login import login_required, login_user
 from .forms import LoginForm, RegistryForm
+
+@auth.route('/login')
+def login():
+    return render_template('auth/login.html')
 
 @auth.route('/user')
 def user():
---
+++ app/models.py
@@ -0,0 +1,39 @@
+from app import db, login_manager
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, index=True, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(128), index=True, unique=True)
+    password = db.Column(db.String(128))
+    realname = db.Column(db.String(64))
+    job = db.Column(db.String(64))
+    Phone = db.Column(db.String(64))
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attr')
+    
+    @password.setter
+    def password(self, pwd):
+        self.password = generate_password_hash(pwd, salt_length=16)
+
+    def verify_password(self, pwd):
+        return check_password_hash(self.password, pwd)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
