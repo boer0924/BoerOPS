921f4d3bdc06972749c67253e881bf21f3b79eb1
boer <boer0924@gmail.com>
2017-07-05 16:57:56 +0800

    modify deploy progress fe

--- app/static/js/projects/deploy.js
+++ app/static/js/projects/deploy.js
@@ -7,7 +7,7 @@ layui.use(['element', 'layer', 'util', 'form', 'layedit', 'laypage'], function (
     var layedit = layui.layedit;
     var laypage = layui.laypage;
  
-    // 添加项目弹出层
+    // 填写信息弹出层
     $('#deploy-info-btn').on('click', function () {
         layer.open({
             type: 1,
@@ -22,17 +22,198 @@ layui.use(['element', 'layer', 'util', 'form', 'layedit', 'laypage'], function (
             // shadeClose: true,
         });
     });
-
-    // 执行绑定
+    // 测试结果弹出层
+    $('#test-result-btn').on('click', function () {
+        layer.open({
+            type: 1,
+            title: '测试结果',
+            skin: 'layui-layer-molv',
+            content: $('#results-ctx'),
+            area: ['320px', '200px'],
+            // area: 'auto',
+            // maxWidth: '900px',
+            closeBtn: 1,
+            resize: true
+            // shadeClose: true,
+        });
+    });
+    // 校验结果弹出层
+    $('#verify-result-btn').on('click', function () {
+        layer.open({
+            type: 1,
+            title: '校验结果',
+            skin: 'layui-layer-molv',
+            content: $('#verify-results-ctx'),
+            area: ['320px', '200px'],
+            // area: 'auto',
+            // maxWidth: '900px',
+            closeBtn: 1,
+            resize: true
+            // shadeClose: true,
+        });
+    });
+    // 发布版本弹出层
+    $('#release-version-btn').on('click', function () {
+        layer.open({
+            type: 1,
+            title: '发布版本',
+            skin: 'layui-layer-molv',
+            content: $('#release-ctx'),
+            area: ['520px', '240px'],
+            // area: 'auto',
+            // maxWidth: '900px',
+            closeBtn: 1,
+            resize: true
+            // shadeClose: true,
+        });
+    });
+    // 提测/上线
     form.on('submit(deployTest)', function(data) {
         if (data.field.environ == 2) {
-            activate(5);
+            activate(3);
         } else {
-            activate(2);
+            activate(1);
         }
         layer.closeAll();
         layer.load();
         // layer.msg(JSON.stringify(data.field));
+        $.ajax({
+            type: 'POST',
+            url: '/projects/deploy',
+            data: data.field,
+            dataType: 'json',
+            // contentType: false,
+            processData: true,
+            success: function (data, txtStatus, jqXHR) {
+                layer.closeAll('loading');
+                if (data.code === 200) {
+                    layer.msg(data.msg, {
+                        icon: 1,
+                        time: 1600,
+                        anim: 0,
+                        shade: [0.6, '#c2c2c2']
+                    },
+                    activate(data.status + 1),
+                    layer.open({
+                        type: 1,
+                        title: '测试结果',
+                        skin: 'layui-layer-molv',
+                        content: $('#results-ctx'),
+                        area: ['320px', '200px'],
+                        closeBtn: 1,
+                        resize: true
+                    })
+                    );
+                } else {
+                    layer.msg(data.msg, {
+                        icon: 2,
+                        time: 2000,
+                        anim: 6,
+                        shade: [0.6, '#c2c2c2']
+                    });
+                }
+            }
+        });
+        return false;
+    });
+    // 测试结果
+    form.on('submit(testResult)', function(data) {
+        layer.msg(JSON.stringify(data.field));
+        if (data.field.result === 'on') {
+            activate(3);
+        } else {
+            activate(6);
+            layer.closeAll('loading');
+            return false;
+        }
+        layer.closeAll();
+        layer.load();
+        $.ajax({
+            type: 'POST',
+            url: '/projects/deploy',
+            data: data.field,
+            dataType: 'json',
+            // contentType: false,
+            processData: true,
+            success: function (data, txtStatus, jqXHR) {
+                layer.closeAll('loading');
+                if (data.code === 200) {
+                    layer.msg(data.msg, {
+                        icon: 1,
+                        time: 1600,
+                        anim: 0,
+                        shade: [0.6, '#c2c2c2']
+                    },
+                    // function () {
+                    //     location.href="/projects/deploy";
+                    // }
+                    // activate(data.status)
+                    activate(3),
+                    $('#verify-result-btn').click(),
+                    );
+                } else {
+                    layer.msg(data.msg, {
+                        icon: 2,
+                        time: 2000,
+                        anim: 6,
+                        shade: [0.6, '#c2c2c2']
+                    });
+                }
+            }
+        });
+        return false;
+    });
+    // 校验结果
+    form.on('submit(verifyResult)', function(data) {
+        layer.msg(JSON.stringify(data.field));
+        if (data.field.result === 'on') {
+            activate(3);
+        } else {
+            activate(6);
+            layer.closeAll('loading');
+            return false;
+        }
+        layer.closeAll();
+        layer.load();
+        $.ajax({
+            type: 'POST',
+            url: '/projects/deploy',
+            data: data.field,
+            dataType: 'json',
+            // contentType: false,
+            processData: true,
+            success: function (data, txtStatus, jqXHR) {
+                layer.closeAll('loading');
+                if (data.code === 200) {
+                    layer.msg(data.msg, {
+                        icon: 1,
+                        time: 1600,
+                        anim: 0,
+                        shade: [0.6, '#c2c2c2']
+                    },
+                    // function () {
+                    //     location.href="/projects/deploy";
+                    // }
+                    // activate(data.status)
+                    activate(5),
+                    $('#release-version-btn').click(),
+                    );
+                } else {
+                    layer.msg(data.msg, {
+                        icon: 2,
+                        time: 2000,
+                        anim: 6,
+                        shade: [0.6, '#c2c2c2']
+                    });
+                }
+            }
+        });
+        return false;
+    });
+    // 发布版本
+    form.on('submit(releaseVersion)', function(data) {
+        layer.closeAll();
+        layer.load();
         $.ajax({
             type: 'POST',
             url: '/projects/deploy',
@@ -54,6 +235,7 @@ layui.use(['element', 'layer', 'util', 'form', 'layedit', 'laypage'], function (
                     // }
                     // activate(data.status)
                     activate(6),
+                    alert('上线完成')
                     );
                 } else {
                     layer.msg(data.msg, {
